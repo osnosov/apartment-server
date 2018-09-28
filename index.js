110c41a2238eb7f4ecf6a5e8679693da35158268
Oleg Nosov <nosovoleg@gmail.com>
2018-09-28 14:09:17 +0300

    add pages 404

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const router = require('./routes');
 
 const app = new Koa();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+    const status = ctx.status || 404;
+    if (status === 404) {
+      ctx.throw(404);
+    }
+  } catch (err) {
+    ctx.status = err.status || 500;
+    if (ctx.status === 404) {
+      ctx.body = { status: 'error', message: 'Page not found' };
+    } else {
+      ctx.body = { status: 'error', message: 'Other error' };
+    }
+  }
+});
+
 app.use(cors()).use(bodyParser());
 
 app.use(passport.initialize());
@@ -20,6 +39,4 @@ app.use(router.routes()).use(router.allowedMethods());
 
 app.on('error', (err, ctx) => console.log(`Server error ${err}, ${ctx}`));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server started on port ${process.env.PORT || ''}`)
-);
+app.listen(PORT, () => console.log(`Server started on port ${PORT || ''}`));
