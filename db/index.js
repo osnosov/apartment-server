9bc97073ca2c4cb500ef4fca41783f3b18db92e1
Oleg Nosov <nosovoleg@gmail.com>
2018-10-07 17:59:53 +0300

    rename environment

--- db/index.js
+++ db/index.js
@@ -1,3 +1,3 @@
-const environment = process.env.ENVIRONMENT || 'development';
-const config = require('../knexfile.js')[environment];
+const env = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[env];
 module.exports = require('knex')(config);
