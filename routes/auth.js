98f265e52f4191e11186906599a36f42b8a856c1
Oleg Nosov <nosovoleg@gmail.com>
2018-10-08 19:18:20 +0300

    User registration added

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,61 @@
 const Router = require('koa-router');
 const passport = require('koa-passport');
+const bcrypt = require('bcrypt');
 
 const { destroySessionsByUserId } = require('../models/sessions');
+const { getUser, createUser } = require('../models/user');
 
 const router = new Router({
   prefix: '/auth',
 });
 
+router.post('/signup', async (ctx, next) => {
+  const user = ctx.request.body;
+
+  if (!user.password || !user.email) {
+    ctx.throw(403, 'You must fill out all fields to signup.');
+  }
+
+  try {
+    const { email } = user;
+    const findUser = await getUser({ email });
+    if (!findUser) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(user.password, salt);
+
+      const newUserId = await createUser({ ...user, password: hash });
+
+      await passport.authenticate('local', (err, token) => {
+        if (token === false) {
+          ctx.throw(403, 'Authentication failed.');
+        } else {
+          ctx.body = {
+            status: 'success',
+            message: 'User has created',
+            user: newUserId[0],
+            token,
+          };
+        }
+      })(ctx, next);
+    } else {
+      ctx.status = 400;
+      ctx.body = { status: 'error', message: 'You have already signed up.' };
+    }
+  } catch (err) {
+    ctx.throw(400, 'Encountered an error');
+  }
+});
+
 router.post('/login', async (ctx, next) => {
+  const user = ctx.request.body;
+
+  if (!user.password || !user.email) {
+    ctx.throw(403, 'You must fill out all fields to login.');
+  }
+
   await passport.authenticate('local', (err, token) => {
     if (token === false) {
-      ctx.status = 403;
-      ctx.body = { status: 'error', message: 'Authentication failed' };
+      ctx.throw(403, 'Authentication failed.');
     } else {
       ctx.body = { status: 'success', message: 'Login success', token };
     }
@@ -24,8 +68,7 @@ router.post('/logout', async (ctx, next) => {
       await destroySessionsByUserId(user.id);
       ctx.body = { status: 'success', message: 'Logout successful' };
     } else {
-      ctx.status = 401;
-      ctx.body = { status: 'error', message: 'Unauthorized' };
+      ctx.throw(401, 'Unauthorized.');
     }
   })(ctx, next);
 });
